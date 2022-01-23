cf9a2294a0ed4829a12e32f63a443e795533e59d
Эдуард Манукян <manukyanedik10@live.com>
2022-01-23 17:14:11 +0300

    Добавьте файлы проекта.

---
+++ diffy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+namespace diffy
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BigInteger g, p, A, B, KA, KB;
+            int a, b;
+            Console.Write("Введите g: ");
+            g = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите p: ");
+            p = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите a: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            b = Int32.Parse(Console.ReadLine());
+            A = BigInteger.Pow(g, a) % p;
+            B = BigInteger.Pow(g, b) % p;
+            KA = BigInteger.Pow(B, a) % p;
+            KB = BigInteger.Pow(A, b) % p;
+            Console.WriteLine("KA = " + KA);
+            Console.WriteLine("KB = " + KB);
+        }
+    }
+}
